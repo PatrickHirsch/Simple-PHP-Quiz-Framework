7eb673608e0fb315123e06b262af0a115545db75
PatrickHirsch <HirschP2@nku.edu>
2026-04-16 11:48:35 -0400

    Initial commit.

---
+++ quizframework.php
@@ -0,0 +1,97 @@
+<?php
+$giveFlag=false;
+$answersPassed=($_SERVER['REQUEST_METHOD']==='POST');
+$results=[];
+
+if($answersPassed)
+{	$giveFlag=true;
+	foreach($questions as $i=>$qa)
+	{	$user_answer=strtoupper(preg_replace('/\s+/',' ',trim($_POST[$i]??'')));
+		$is_correct=in_array($user_answer,$qa['a'],true);
+		$results[$i]=$is_correct;
+		if(!$is_correct) $giveFlag=false;
+    }
+}
+?>
+
+<html>
+<head>
+	<title><?=htmlspecialchars($challengeName??'',ENT_QUOTES,'UTF-8')?></title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+
+
+<?php if($giveFlag): ?>
+	<div class="flag">
+		<p>
+			<?= htmlspecialchars($theFLAG,ENT_QUOTES,'UTF-8'); ?>
+		</p>
+	</div>
+
+
+
+
+<?php else: ?>
+	<div>
+		<div><p>&nbsp;</p></div>
+	</div>
+<div class="formcontainer">
+	<form method="POST" action="">
+		<h1><?=htmlspecialchars($challengeName??'',ENT_QUOTES,'UTF-8')?></h1>
+		<hr>
+		<?php foreach ($questions as $i => $qa): ?>
+			<?php
+			// Determine if answer was correct on previous submission:
+				$userScore='';
+				if($answersPassed) $userScore=!empty($results[$i])?'correct':'incorrect';
+			?>
+			
+			<label for="<?= $i ?>">
+				<?= $qa['q'];  "\\ <--- Line not escaped, subject to injection attacks, I am assuming my questions are secure; I am doing this " ?>
+			</label>
+			
+			<br>
+			
+			<input
+				type="text"
+				id="<?=$i?>"
+				name="<?=$i?>"
+				class="<?=$userScore?>"
+				value="<?=htmlspecialchars($_POST[$i]??'',ENT_QUOTES,'UTF-8')?>"
+			>
+			<hr>
+		<?php endforeach; ?>
+	
+		<button type="submit">Check Answers</button>
+	</form>
+</div>
+<?php endif; ?>
+
+</body>
+</html>
+
+<script>
+// https://medium.com/@a1guy/save-and-restore-scroll-position-in-javascript-no-libraries-needed-c38688b977fa
+//	let timeout;
+//	window.addEventListener("scroll", () => {
+//	  clearTimeout(timeout);
+//	  timeout = setTimeout(() => {
+//	    localStorage.setItem("scrollY", window.scrollY);
+//	  }, 200); // Debounce to avoid saving too frequently
+//	});
+document.querySelector('form').addEventListener('submit',
+	function()
+	{	localStorage.setItem('scrollY',window.scrollY);
+	}
+);
+
+window.addEventListener("load", () => {
+  const savedY = localStorage.getItem("scrollY");
+  if (savedY !== null) {
+    window.scrollTo(0, parseInt(savedY));
+  }
+});
+</script>
---
+++ template.php
@@ -0,0 +1,25 @@
+<?php
+
+$challengeName="Sample";
+$theFLAG="cybershield{TEMPLATE}";
+$questions=
+[	"q1" => [
+		"q" => "question 1",
+		"a" => ["POSSIBLE ANSWER 1", "POSSIBLE ANSWER 2", "POSSIBLE ANSWER 3"]
+	],
+	"q2" => [
+		"q" => "question 2",
+		"a" => ["POSSIBLE ANSWER 1", "POSSIBLE ANSWER 2", "POSSIBLE ANSWER 3"]
+	],
+	"q3" => [
+		"q" => "question 3",
+		"a" => ["POSSIBLE ANSWER 1", "POSSIBLE ANSWER 2", "POSSIBLE ANSWER 3"]
+	],
+];
+
+
+?>
+
+<?php 
+include 'quizframework.php';
+?>
